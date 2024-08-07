7148f5b8ee4044da8c8e0e0e81ed489b29e85f45
JeanLeonel <jean.leonel.costa@gmail.com>
2024-08-07 19:43:06 -0300

    first steps in Java With SpringBoot

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-java/src/main/java/br/com/jc/Greeting.java
@@ -0,0 +1,43 @@
+package br.com.jc;
+
+import java.util.Objects;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Greeting greeting = (Greeting) o;
+        return id == greeting.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
