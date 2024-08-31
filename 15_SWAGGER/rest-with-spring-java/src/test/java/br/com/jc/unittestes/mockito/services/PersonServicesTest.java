766902340f0ac21e6a38cfd3d28144277ad7b243
Jean Leonel <jean.leonel.costa@gmail.com>
2024-08-31 12:12:50 -0300

    testes unitarios de book

--- 15_SWAGGER/rest-with-spring-java/src/test/java/br/com/jc/unittestes/mockito/services/PersonServicesTest.java
+++ 15_SWAGGER/rest-with-spring-java/src/test/java/br/com/jc/unittestes/mockito/services/PersonServicesTest.java
@@ -1,4 +1,4 @@
-package br.com.jc.unittests.mockito.services;
+package br.com.jc.unittestes.mockito.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
